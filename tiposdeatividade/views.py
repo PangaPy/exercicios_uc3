6629655e48d7ea6c3f373855f58a4ee6573a2052
Douglas Silva <Douglas_silva94@hotmail.com>
2025-05-16 21:32:41 -0300

    projeto_django

--- tiposdeatividade/views.py
+++ tiposdeatividade/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("olá! Eu sou o index")
+
+def exibe_mensagem(request):
+    t_html=<!DOCTYPE html><html lang="pt-BR"><head><meta charset="UTF-8"><meta name="viewport"content="width=device-width,initial-scale=1.0"><title>Escola</title></head><body><p>Esta é minha página de índice</p></body></html>
+
+    return HttpResponse(t_html)
+

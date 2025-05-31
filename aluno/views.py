ccf1ee8755a9d2c35bd96b04b56e8d38418fafd0
Douglas Silva <Douglas_silva94@hotmail.com>
2025-05-30 21:38:45 -0300

    projeto_django

--- aluno/views.py
+++ aluno/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def listar(request):
     return render(request,'aluno/listarAlunos.html')
+
+def cadastrar(request): 
+    return render(request,'aluno/cadastroAluno.html')
     
--- instrutor/urls.py
+++ instrutor/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views 
 
+app_name = 'instrutor'
+
 urlpatterns = [
-    path('', views.index, name="instrutor"),
+    path('cadastrar', views.cadastrar, name="cadastrar"),
+    path('listar', views.listar, name= "listar"),
 ]

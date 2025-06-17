07635718f6aa49461e529e77c0ad64e02f6e1ff4
Douglas Silva <Douglas_silva94@hotmail.com>
2025-06-16 21:34:00 -0300

    projeto_django

--- titulo/views.py
+++ titulo/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import redirect, render
-from titulo.forms import TituloForm
+from django.shortcuts import render, redirect
+
 from titulo.models import Titulo
+from titulo.forms import TituloForm, TituloAtualizarForm
 from instrutor.models import Instrutor
+
 
 # Create your views here.
 def listar(request):
@@ -13,24 +15,64 @@ def listar(request):
 
     return render(request, 'titulo/listarTitulos.html', context=contexto)
 
+
+# carregar a pagina para cadastrar um titulo na base de dados
 def cadastro(request):
     return render(request, 'titulo/cadastroTitulos.html')
 
+
+# acao de cadastrar um titulo na base de dados
 def cadastrar(request):
     form = TituloForm(request.POST)
     if form.is_valid():
         dados_titulo = form.cleaned_data
-        titulo = Titulo(descricao = dados_titulo['descricao']
+        titulo = Titulo(
+            descricao = dados_titulo['descricao']
         )
         titulo.save()
 
     return render(request, 'titulo/cadastroTitulos.html')
 
+
+# excluir um titulo
 def excluir(request, codigo):
     titulo = Titulo.objects.get(pk=codigo)
 
+    # verifica se ha um instrutor relacionado ao tiutlo a excluir
+    # caso contrario, o titulo poderá ser excluido
     instrutores = Instrutor.objects.filter(codigo_titulo=codigo)
-    if not instrutores: 
+    if not instrutores:
         titulo.delete()
+
+    return redirect('titulo:listar')
+
+# carregar titulo para atualizar
+def carregar_titulo(request, codigo):
+    # obter titulo a atualizar basead no codigo informado
+    titulo = Titulo.objects.get(pk=codigo)
+    contexto = {
+        'titulo': titulo,
+    }
+
+    return render(request, 'titulo/atualizarTitulo.html', context=contexto)
+
+# atualizar a base de dados para o titulo selecionado
+def atualizar(request):
+    if request.method == 'POST':
+        form = TituloAtualizarForm(request.POST)
+        if form.is_valid():
+
+            dados_titulo = form.cleaned_data
+
+            codigo = dados_titulo['codigo']
+
+            titulo = Titulo.objects.get(pk=codigo)
+            titulo.descricao = dados_titulo['descricao']
+
+            titulo.save()
+        
+        # imprimir no console mensagens de erro na validação do formulário
+        #else:
+        #    print(form.errors)
 
     return redirect('titulo:listar')
